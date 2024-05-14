2cf7f549f5635872ce543e902cd2556fddcc4e0c
stnwtr <simon.steinkellner.ss@gmail.com>
2024-05-14 17:05:31 +0200

    Mount static files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, keksi!");
+use axum::{Router, serve};
+use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
+
+#[tokio::main]
+pub async fn main() -> anyhow::Result<()> {
+    let router = Router::new()
+        .route_service("/", ServeDir::new("html"));
+
+    serve(TcpListener::bind("127.0.0.1:8080").await?, router).await?;
+
+    Ok(())
 }
